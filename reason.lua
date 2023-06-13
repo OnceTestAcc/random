2386914fe2927b083edcceef5220d5e014acaab0
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2023-06-13 16:43:46 -0400

    Update reason.lua

--- reason.lua
+++ reason.lua
@@ -1,1 +1,1 @@
-return 'Fun Fact: Wicky-Picky#1524 username is "DFDERD41. Outdated script contact rgie#2155 for updated script."'
+return 'Fun Fact: Wicky-Picky#1524 username is "DFDERD41. If you got kicked you have the outdated script contact rgie#2155 for updated script."'
